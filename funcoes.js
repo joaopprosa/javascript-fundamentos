16b26cae224b9de86ca2970e08490c126801a513
Joao Rosa <joao.rosa@gec.inatel.br>
2022-01-08 14:30:22 -0300

    funcoes

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,33 @@
+//Funcao sem retorno
 function imprimirSoma(a, b){
     console.log(a + b)
 }
 
 imprimirSoma(2, 3)
+imprimirSoma(2)
+
+//Funcao com retorno
+function soma(a, b=0){
+    return a + b
+}
+
+console.log(soma(4, 5))
+
+//Armazenando uma funcao em uma variavel
+const imprimirSoma2 = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma2(6, 7)
+
+//Armazenando uma funcao arrow em uma variavel
+const soma2 = (a, b) => {
+    return a + b
+}
+
+console.log(soma2(8, 8))
+
+//Retorno implicito
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(5, 3))
